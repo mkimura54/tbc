a85e99df258f575829c5f4d76e569fc4b37ede02
Masashi Kimura <screw@mtg.biglobe.ne.jp>
2021-06-18 21:52:12 +0900

    add interactive mode

--- main.go
+++ main.go
@@ -3,25 +3,53 @@ package main
 import (
 	"os"
 	"fmt"
+	"strings"
+	"bufio"
 )
 
 func main() {
 	l := len(os.Args)
-	if l == 2 {
-		result, err := convert(os.Args[1])
-		if err != nil {
-			fmt.Printf("%s\n", err)
-		} else {
-			fmt.Println(result)
+	if l == 1 {
+		for {
+			sc := bufio.NewScanner(os.Stdin)
+			for sc.Scan() {
+				input := sc.Text()
+				if input == "quit" || input == "exit" || input == "q" {
+					return
+				} else {
+					param := strings.Split(input, " ")
+					count := len(param)
+					if count == 1 {
+						execConvert(param[0])
+					} else if count == 2 {
+						execSubtraction(param[0], param[1])
+					}
+				}
+			}
 		}
+	} else if l == 2 {
+		execConvert(os.Args[1])
 	} else if l == 3 {
-		result, err := subtraction(os.Args[1], os.Args[2])
-		if err != nil {
-			fmt.Printf("%s\n", err)
-		} else {
-			fmt.Println(result)
-		}
+		execSubtraction(os.Args[1], os.Args[2])
 	} else {
 		fmt.Println("param count error.")
+	}
+}
+
+func execConvert(param string) {
+	result, err := convert(param)
+	if err != nil {
+		fmt.Printf("%s\n", err)
+	} else {
+		fmt.Println(result)
+	}
+}
+
+func execSubtraction(param1, param2 string) {
+	result, err := subtraction(param1, param2)
+	if err != nil {
+		fmt.Printf("%s\n", err)
+	} else {
+		fmt.Println(result)
 	}
 }
